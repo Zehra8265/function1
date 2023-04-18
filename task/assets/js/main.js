d95cbf82b0c91ea581d762070d98080ca5b4c23b
Zehra <127186223+Zehra8265@users.noreply.github.com>
2023-04-18 06:57:34 -0300

    Add files via upload

---
+++ task/assets/js/main.js
@@ -0,0 +1,46 @@
+// function sahe(a,b){
+//     var  a,b;
+//     return a*b
+// }
+// var d=sahe(6,5)
+// console.log(d);
+
+
+// function sahe (a,b,c){
+// var a,b,c;
+// return 2*(a*b+b*c+a*c)
+// }
+// var d=sahe (4,5,6)
+// console.log(d);
+
+// function sahe (a,b,c,r){
+// var p=(Number(a)+Number(b)+Number(c))/2;
+// return p*r
+// }
+// console.log(sahe(prompt(),prompt(),prompt(),prompt()));
+
+
+//    function eded(a){
+//    if(a>9 && a<100){
+//     var teklik=a%10
+//     var onluq=(a-teklik)/10
+    
+//    }
+//    return onluq + " " +teklik;
+
+//    }
+//    var d= eded (12)
+//    console.log(d);
+
+
+
+
+// function FindIndex(str,chr){
+//     for(let i=0; i<str.length; i++){
+//         if(str[i]===chr){
+//             return i;
+//         }
+//     }
+//     return -1
+// }
+// console.log(FindIndex("salam","l"));
